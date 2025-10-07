d3c2e150f20a9d1a427a4a74b344568d2f4863c6
Alfrey-Chan <122188063+Alfrey-Chan@users.noreply.github.com>
2025-10-07 13:05:42 -0700

    remove test route and code

--- app/Http/Controllers/Api/RequestController.php
+++ app/Http/Controllers/Api/RequestController.php
@@ -55,11 +55,11 @@ class RequestController extends Controller
             }
         }
 
-        Mail::to(config('mail.admin_email'))
-            ->send(new RequestSubmissionMail($requestSubmission));
+        // Mail::to(config('mail.admin_email'))
+        //     ->send(new RequestSubmissionMail($requestSubmission));
 
-        Mail::to($requestSubmission->email)
-            ->send(new RequestReceivedMail($requestSubmission));
+        // Mail::to($requestSubmission->email)
+        //     ->send(new RequestReceivedMail($requestSubmission));
 
         return response()->json([
             "message" => "Request submitted successfully!",
